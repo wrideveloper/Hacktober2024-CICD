6c819f9ca60efb6721e6c34269c32e1c541d7743
FlaBBB <f12345ff67@gmail.com>
2024-10-26 00:48:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ✨ Hacktober 2024 Day-1 
+
+Hari pertama hacktoberfest'24 di WRI, disini kita bakal belajar colaboration, CI/CD, dan lain-lain. 🔥
+
+# 🏎️ CI/CD 
+
+CI/CD adalah singkatan dari Continuous Integration dan Continuous Deployment. CI/CD adalah metode pengembangan perangkat lunak yang memungkinkan pengembang untuk menggabungkan kode ke dalam repositori bersama secara berkala, kemudian mengotomatisasi proses pembangunan dan pengujian kode, serta penyebaran kode ke server produksi.
+
+# 🤝 Contributors
+
+- [Fikri Muhammad Abdillah](https://github.com/FlaBBB)
---
+++ astro.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import UnoCSS from 'unocss/astro';
+
+export default defineConfig({
+  // used to generate images
+  site:
+    process.env.VERCEL_ENV === 'production'
+      ? 'https://FlaBBB.github.io/'
+      : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/`
+      : 'https://localhost:3000/',
+  base: 'SCnPRnCICD',
+  trailingSlash: 'ignore',
+  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  vite: {
+    optimizeDeps: {
+      exclude: ['@resvg/resvg-js'],
+    },
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+});
